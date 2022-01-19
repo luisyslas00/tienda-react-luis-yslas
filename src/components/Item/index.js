d84e674186551af9161bc645b4c65f39e4114f0c
luisyslas00 <yslasluis92@gmail.com>
2022-01-19 16:53:45 -0300

    Tienda Luis Yslas, primer arreglo

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -15,18 +15,3 @@ import "../Item/style.css"
  }
 
  export default Item;
-
-// function Item(props){
-//     return(
-//         <div className="productContainer">
-//             <p className="productContainer__nombre">{props.nombre}</p>
-//             <img src={props.imagen}/>
-//             <p className="productContainer__precio">Precio: ${props.precio}</p>
-//             <p className="productContainer__detalles">{props.detalles}</p>
-//             <ItemCount/>
-//             <button onClick={props.id} className="productContainer__btn">Agregar Al Carrito</button>
-//         </div>
-//     )
-// }
-
-// export default Item;
