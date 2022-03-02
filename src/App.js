2f81cc49c6f70379a245ae2f3e5399a75750ee1b
luisyslas00 <yslasluis92@gmail.com>
2022-03-02 13:21:48 -0300

    Home

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <BrowserRouter>
         <Navbar/>
         <Routes>
-          <Route path="/"/>
+          <Route path="/tienda-react-luis-yslas"/>
             <Route index element={<HomePage/>} />
             <Route path="about" element={<AboutPage/>}/>
             <Route path="products">
